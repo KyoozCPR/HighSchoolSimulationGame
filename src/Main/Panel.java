66760a3453e054fa52f096e596ef1193c5d70d64
Diego Computer <cipriettid949@gmail.com>
2025-03-08 12:48:51 +0100

    implemented delta time for consistent movement across different framerates

--- src/Main/Panel.java
+++ src/Main/Panel.java
@@ -18,7 +18,7 @@ public class Panel extends JPanel implements Runnable{
     private int squareH = 20;
     Thread gameThread;
     KeyHandler kHandler;
-
+    long waitTime;// delta time
     public Panel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
@@ -39,7 +39,7 @@ public class Panel extends JPanel implements Runnable{
     public void run() {
         this.add(new JLabel("Ciao!"));
         // measure the time each frame takes, then sleep for the remaining time until 16.67 ms have elapsed.
-        final double timePerFrame = 1_000_000_000 / 60.0; // 16.67 ms for 60FPS
+        final double timePerFrame = 1_000_000_000 / 60.0;// 16.67 ms for 60FPS
         while (gameThread != null){
             long startTime = System.nanoTime();
 
@@ -47,7 +47,7 @@ public class Panel extends JPanel implements Runnable{
             repaint();
             // frame rendered so now we measure how much time it took
             long elapsed = System.nanoTime() - startTime;
-            long waitTime = (long)(timePerFrame - elapsed) / 1_000_000; // convert to ms
+            waitTime = (long)(timePerFrame - elapsed) / 1_000_000; // convert to ms
 
             if (waitTime < 0) {
                 waitTime = 5; // minimal wait to prevent tight looping if behind schedule
@@ -65,14 +65,15 @@ public class Panel extends JPanel implements Runnable{
 
     public void update(){
         System.out.println(squareX + "," + squareY);
+        double deltaTime = waitTime / 10.0;
         if (kHandler.upPressed)
-            squareY -= 5 ;
+            squareY -= 5 * deltaTime;
         if (kHandler.rightPressed)
-            squareX += 5;
+            squareX += 5 * deltaTime;
         if (kHandler.leftPressed)
-            squareX -= 5;
+            squareX -= 5 * deltaTime;
         if (kHandler.downPressed)
-            squareY += 5;
+            squareY += 5 * deltaTime;
     }
 
     @Override
