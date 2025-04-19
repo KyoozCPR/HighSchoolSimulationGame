96e5f61586bf82d4dad347132bed9e3367080e79
Diego Computer <cipriettid949@gmail.com>
2025-04-20 01:51:14 +0200

    Uplifts basic sprites

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -1,15 +1,38 @@
 package Tile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class TileManager {
     public Tile[] tileset;
     public Tile[][] tilemap;
 
-    public TileManager() {
-        getTileset();
-        getTileMap();
+    public TileManager() throws IOException {
+        setTileset();
+        setTileMap();
     }
 
-    private void getTileSet(){
+
+    // retrieve tile images from spritesheet.png by separating them into 32x32px tiles
+    private void setTileset() throws IOException {
+        this.tileset = new Tile[10];
+        File[] tilesDirectory = new File("src/Maps").listFiles();
+
+        for (int i=0; i<tileset.length; i++){
+            BufferedImage image  = new BufferedImage(
+                    32*3, 32*3, BufferedImage.TYPE_INT_ARGB);
+            try {
+                image = ImageIO.read(tilesDirectory[i].getAbsoluteFile());
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+            tileset[i] = new Tile(image);
+
+        }
 
     }
 }
